3efed64a3befd3f6c03adb4d843b29e473533553
Ricky Taylor <rickytaylor26@gmail.com>
2021-02-17 00:20:05 +0000

    Make build less tied to my system

--- examples/megapong/img/make.sh
+++ examples/megapong/img/make.sh
@@ -2,20 +2,17 @@
 set -e
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-HOST=x86_64-unknown-linux-gnu
+[ -z "$LLVM_CONFIG" ] && LLVM_CONFIG=$(which llvm-config)
+
 TARGET=m68k-none-eabi
-ROOT_DIR=~/Projects/OSS/m68k
-LLVM_BIN=$(realpath $ROOT_DIR/llvm/build-relwithdebinfo/bin)
-RUSTDIR=$(realpath $ROOT_DIR/rust/build/$HOST)
-export RUSTC=$RUSTDIR/stage2/bin/rustc
-CARGO=$RUSTDIR/stage2-tools-bin/cargo
+LLVM_BIN=$("$LLVM_CONFIG" --bindir)
 CLANG=$LLVM_BIN/clang
 LD=$LLVM_BIN/ld.lld
 OBJCOPY=$LLVM_BIN/llvm-objcopy
 TARGET_OUT=../../../target/$TARGET/release
 
 pushd ..
-$CARGO -Z build-std=core build --release --target .cargo/$TARGET.json -v
+cargo -Z build-std=core build --release --target .cargo/$TARGET.json
 popd
 $CLANG -target $TARGET -c entry.S
 $LD --gc-sections -o img.elf -Ttext=0 entry.o $TARGET_OUT/libmegapong.rlib $TARGET_OUT/deps/libcore-*.rlib $TARGET_OUT/deps/libcompiler_builtins-*.rlib
