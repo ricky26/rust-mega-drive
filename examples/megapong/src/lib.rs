8f2518ce2420536cd67b2b185e733182e1c441fa
Ricky Taylor <rickytaylor26@gmail.com>
2021-02-22 23:39:30 +0000

    Add FM test to example

--- examples/megapong/src/lib.rs
+++ examples/megapong/src/lib.rs
@@ -3,6 +3,7 @@
 
 use core::panic::PanicInfo;
 use megadrive_sys::vdp::{VDP, Sprite, SpriteSize};
+use megadrive_sys::fm::{FM, Note, Panning};
 use megadrive_input::Controllers;
 use core::ptr::{read_volatile, write_volatile};
 
@@ -15,9 +16,11 @@ extern "C" {
 #[no_mangle]
 pub fn main() -> ! {
     loop {
+        let mut fm = FM::new();
         let vdp = VDP::new();
         let mut controllers = Controllers::new();
 
+        // Load graphics.
         static TILE_DATA: [u8; 256] = [
             // H
             0x01, 0x00, 0x01, 0x00,
@@ -94,11 +97,53 @@ pub fn main() -> ! {
         ];
         vdp.set_tiles(1, TILE_DATA.array_chunks());
 
+        // Setup synth.
+        fm.set_multiplier(0, 0, 1, 7);
+        fm.set_multiplier(0, 1, 13, 0);
+        fm.set_multiplier(0, 2, 3, 3);
+        fm.set_multiplier(0, 3, 1, 0);
+
+        fm.set_total_level(0, 0, 35);
+        fm.set_total_level(0, 1, 45);
+        fm.set_total_level(0, 2, 38);
+        fm.set_total_level(0, 3, 0);
+
+        fm.set_attack_rate(0, 0, 63, 1);
+        fm.set_attack_rate(0, 1, 25, 2);
+        fm.set_attack_rate(0, 2, 63, 1);
+        fm.set_attack_rate(0, 3, 20, 2);
+
+        fm.set_decay_rate(0, 0, 5, false);
+        fm.set_decay_rate(0, 1, 5, false);
+        fm.set_decay_rate(0, 2, 5, false);
+        fm.set_decay_rate(0, 3, 7, false);
+
+        fm.set_sustain_rate(0, 0, 2);
+        fm.set_sustain_rate(0, 1, 2);
+        fm.set_sustain_rate(0, 2, 2);
+        fm.set_sustain_rate(0, 3, 2);
+
+        fm.set_release_rate(0, 0, 1, 1);
+        fm.set_release_rate(0, 1, 1, 1);
+        fm.set_release_rate(0, 2, 1, 1);
+        fm.set_release_rate(0, 3, 6, 10);
+
+        fm.set_algorithm(0, 2, 6);
+        fm.set_panning(0, Panning::Both, 0, 0);
+        fm.set_frequency(0, Note::F, 5);
+
         let mut frame = 0u16;
         loop {
             controllers.update();
             let c1 = controllers.controller_state(0);
             let buttons = c1.map_or(0, |c| c.get_down_raw());
+
+            // Make music.
+            match frame & 127 {
+                0 => fm.set_key(0, true),
+                64 => fm.set_key(0, false),
+                _ => {},
+            }
 
             // Write sprites
             let mut x = 200;
