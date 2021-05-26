75bb120509413bbba6361f2a208634790384873c
reinvantveer <reinvantveer@gmail.com>
2021-05-26 18:31:58 +0200

    add default megadrive stuff
    Signed-off-by: reinvantveer <reinvantveer@gmail.com>
    Signed-off-by: reinvantveer <rein@vantveer.me>

--- examples/megatowerdefense/src/lib.rs
+++ examples/megatowerdefense/src/lib.rs
@@ -7,19 +7,39 @@ use rand::{Rng, SeedableRng};
 use rand::rngs::SmallRng;
 use rand::RngCore;
 
-use megadrive_input::{Controllers, Button};
+use megadrive_input::Controllers;
 use megadrive_graphics::Renderer;
+
+static mut NEW_FRAME: u16 = 0;
+
+extern "C" {
+    fn wait_for_interrupt();
+}
 
 #[no_mangle]
 pub fn main() -> ! {
     let mut renderer = Renderer::new();
     let mut controllers = Controllers::new();
-    let mut small_rng = SmallRng::seed_from_u64(42);
 
     loop {
         renderer.clear();
         controllers.update();
 
-        let random_number = small_rng.next_u32();
+        let mut small_rng = SmallRng::seed_from_u64(42);
+        let _random_number = small_rng.next_u32();
     }
+}
+
+fn wait_for_vblank() {
+    unsafe {
+        while read_volatile(&NEW_FRAME) == 0 {
+            wait_for_interrupt();
+        }
+        NEW_FRAME = 0;
+    }
+}
+
+#[no_mangle]
+fn vblank() {
+    unsafe { write_volatile(&mut NEW_FRAME, 1) };
 }
