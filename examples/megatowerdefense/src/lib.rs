3ef33f946a041e3ce7ea1ef92fee911aca1a9f00
reinvantveer <reinvantveer@gmail.com>
2021-06-08 22:09:01 +0200

    add struct to generate random numbers from horizontal and vertical graphics counter
    Signed-off-by: reinvantveer <reinvantveer@gmail.com>
    Signed-off-by: reinvantveer <rein@vantveer.me>

--- examples/megatowerdefense/src/lib.rs
+++ examples/megatowerdefense/src/lib.rs
@@ -3,29 +3,53 @@
 use core::panic::PanicInfo;
 use core::ptr::{read_volatile, write_volatile};
 
-use rand::{Rng, SeedableRng};
-use rand::rngs::SmallRng;
-
 use megadrive_input::Controllers;
 use megadrive_graphics::Renderer;
 
-static mut NEW_FRAME: u16 = 0;
+static mut NEW_FRAME: u16     = 0;
+const GFX_HVCOUNTER_PORT: u32 = 0xC00008;
 
 extern "C" {
     fn wait_for_interrupt();
 }
 
+struct PseudoRng {
+    seed: u16,
+    current_rand:  u16,
+}
+
+impl PseudoRng {
+    // Thank you Stephane Dallongeville!
+    pub fn from_seed(seed: u16) -> PseudoRng {
+        PseudoRng {
+            seed,
+            current_rand: seed ^ 0xD94B // XOR with some val to avoid 0
+        }
+    }
+
+    pub unsafe fn random(&mut self) -> u16 {
+        self.current_rand = self.current_rand >> 1;
+        let hv_counter = read_volatile(&GFX_HVCOUNTER_PORT) as u16;
+        self.current_rand = self.current_rand ^ hv_counter;
+        self.current_rand << 1
+    }
+}
+
+
+
 #[no_mangle]
 pub fn main() -> ! {
     let mut renderer = Renderer::new();
     let mut controllers = Controllers::new();
+    let mut rng = PseudoRng::from_seed(42);
 
     loop {
         renderer.clear();
         controllers.update();
 
-        let mut small_rng = SmallRng::seed_from_u64(42);
-        let _random_number: u8 = small_rng.gen_range(0..10);
+        unsafe {
+            let _random_number = rng.random();
+        }
     }
 }
 
