17ca6ffa5b451ad8724d4fe2225183e551575e47
reinvantveer <reinvantveer@gmail.com>
2021-07-17 21:30:13 +0200

    remove the target to remove old incremental builds (they build up pretty fast)
    Signed-off-by: reinvantveer <reinvantveer@gmail.com>

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
-set -eux pipefail
+set -eux
+
+# Check whether any cli args were supplied
+if [[ "$#" -ne 0 ]]; then
+  # If so, then if the arg is 'rm' then throw away the previously built target
+  if [[ $1 == 'rm' ]]; then
+    rm -rf target
+  fi
+fi
+
 docker build -t rust-mega-drive:latest .
 docker run -it -v $(pwd)/target:/target rust-mega-drive:latest
 sudo chown -R $USER:$USER target
