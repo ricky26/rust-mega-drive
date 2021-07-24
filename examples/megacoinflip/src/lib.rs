2b53fae8ac82e694319e886fbaa65ff373fba716
reinvantveer <reinvantveer@gmail.com>
2021-07-24 16:35:11 +0200

    drop redundant imports, replace hand-built text with default font
    Signed-off-by: reinvantveer <reinvantveer@gmail.com>
    Signed-off-by: reinvantveer <rein@vantveer.me>

--- examples/megacoinflip/src/lib.rs
+++ examples/megacoinflip/src/lib.rs
@@ -9,57 +9,26 @@ use megadrive_graphics::default_ascii::DEFAULT_FONT_1X1;
 
 static mut NEW_FRAME: u16 = 0;
 
-extern "C" {
-    fn wait_for_interrupt();
-}
-
-fn upload_graphics(vdp: &mut VDP) {
-    // Load graphics.
-    static TILE_DATA: &'static [Tile] = &[
-        // 0 @ 1
-        [
-            0x00, 0x00, 0x08, 0x00,
-            0x08, 0x08, 0x08, 0x00,
-            0x08, 0x00, 0x08, 0x00,
-            0x08, 0x00, 0x08, 0x00,
-            0x08, 0x00, 0x08, 0x00,
-            0x08, 0x00, 0x08, 0x00,
-            0x00, 0x08, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x00,
-        ],
-        // 1 @ 2
-        [
-            0x00, 0x08, 0x00, 0x00,
-            0x08, 0x08, 0x00, 0x00,
-            0x00, 0x08, 0x00, 0x00,
-            0x00, 0x08, 0x00, 0x00,
-            0x00, 0x08, 0x00, 0x00,
-            0x00, 0x08, 0x00, 0x00,
-            0x08, 0x08, 0x08, 0x00,
-            0x00, 0x00, 0x00, 0x00,
-        ],
-    ];
-    vdp.set_tiles(1, TILE_DATA);
-}
-
-
 #[no_mangle]
 pub fn main() -> ! {
     let mut renderer = Renderer::new();
     let mut controllers = Controllers::new();
     let mut vdp = VDP::new();
+
     let mut rng = PseudoRng::from_seed(42);
-    upload_graphics(&mut vdp);
 
     let resolution = vdp.resolution();
-    let half_screen_width = (resolution.0 >> 1) as i16;
-    let half_screen_height = (resolution.1 >> 1) as i16;
+    let half_screen_width = resolution.0 >> 1;
+    let half_screen_height = resolution.1 >> 1;
 
     let x_off = 128 + half_screen_width;
     let y_off = 128 + half_screen_height;
 
     vdp.enable_interrupts(false, true, false);
     vdp.enable_display(true);
+
+    // Load the font tiles
+    DEFAULT_FONT_1X1.load(&mut vdp);
 
     loop {
         renderer.clear();
@@ -70,13 +39,7 @@ pub fn main() -> ! {
 
         let heads_or_tails_tile_idx = flipped + 1;
 
-        let mut sprite = Sprite::with_flags(
-            TileFlags::for_tile(heads_or_tails_tile_idx, 0),
-            SpriteSize::Size1x1);
-
-        sprite.x = x_off as u16;
-        sprite.y = y_off as u16;
-        renderer.draw_sprite(sprite);
+        DEFAULT_FONT_1X1.blit_text(&mut renderer, heads_or_tails, x_off, y_off);
 
         renderer.render(&mut vdp);
         // vsync
