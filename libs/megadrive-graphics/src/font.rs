31d251fd62d003d2d2ba530d923d82d3e3b4d2c0
reinvantveer <reinvantveer@gmail.com>
2021-07-25 11:17:25 +0200

    parameterize h offset by sprite width
    Signed-off-by: reinvantveer <reinvantveer@gmail.com>

--- libs/megadrive-graphics/src/font.rs
+++ libs/megadrive-graphics/src/font.rs
@@ -17,13 +17,20 @@ impl Font {
     /// Displays text using renderer at position (x, y)
     /// Note remember to call renderer.render() afterwards
     pub fn blit_text(&self, renderer: &mut Renderer, text: &str, x: u16, y: u16) {
+        // Calculate sprite offsets. The sprite is in the upper two bits of the sprite size, which
+        // is "zero-indexed", starting at 0b00XX
+        let sprite_width = ((self.sprite_size as u16 & 0b1100) >> 2) + 1;
+        // Convert size 1X, 2X etc. to pixels
+        let sprite_width_as_pixels = sprite_width * 8;
+
         for (idx, byte) in text.as_bytes().into_iter().enumerate() {
             let char_as_tile_idx = *byte as u16 + self.start_index;
 
             let mut sprite = Sprite::with_flags(
                 TileFlags::for_tile(char_as_tile_idx as u16, 0),
                 self.sprite_size);
-            sprite.x = x + 9 * idx as u16;
+
+            sprite.x = x + sprite_width_as_pixels * idx as u16;
             sprite.y = y as u16;
             sprite.set_priority(true);
 
