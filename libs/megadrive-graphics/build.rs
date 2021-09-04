97d3ab1125f1c1e39dd4d3b4146dbd77a122c400
reinvantveer <reinvantveer@gmail.com>
2021-09-04 19:59:21 +0200

    use specific python3 command instead of legacy python
    Signed-off-by: reinvantveer <reinvantveer@gmail.com>
    Signed-off-by: reinvantveer <rein@vantveer.me>

--- libs/megadrive-graphics/build.rs
+++ libs/megadrive-graphics/build.rs
@@ -2,15 +2,15 @@ use std::process::Command;
 
 fn main() {
     println!("cargo:rerun-if-changed=src/default_ascii.rs");
-    Command::new("python")
+    Command::new("python3")
         .args(&["-m", "pip", "install", "--user", "pipenv"])
         .status()
         .unwrap();
-    Command::new("python")
+    Command::new("python3")
         .args(&["-m", "pipenv", "install"])
         .status()
         .unwrap();
-    Command::new("python")
+    Command::new("python3")
         .args(&["-m", "pipenv", "run", "python", "default_font.py"])
         .status()
         .unwrap();
